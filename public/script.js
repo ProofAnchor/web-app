08445c36a4b04799f16206ccc6554191ab79ff22
Jake <jiv7145@gmail.com>
2023-10-19 19:24:04 -0700

    added style and implement storeProof, getProof

--- public/script.js
+++ public/script.js
@@ -1,6 +1,8 @@
+let web3 = new Web3(window.ethereum);
+
 async function connectMetaMask() {
     // Initialize web3 instance
-    let web3 = new Web3(window.etherium);
+    
 
     try {
         // Request account access
@@ -15,6 +17,83 @@ async function connectMetaMask() {
         // Handle error. Likely the user rejected the login
         console.error("Error:", error);
     }
+}
+
+const contractABI = [
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "bytes32",
+        "name": "documentHash",
+        "type": "bytes32"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "timestamp",
+        "type": "uint256"
+      }
+    ],
+    "name": "ProofCreated",
+    "type": "event"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "bytes32",
+        "name": "documentHash",
+        "type": "bytes32"
+      }
+    ],
+    "name": "storeProof",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "bytes32",
+        "name": "documentHash",
+        "type": "bytes32"
+      }
+    ],
+    "name": "getProof",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+]
+
+const contractAddress = '0x289EeF0cDa86fE61fb60f0A95C1b4A2295Dedbee'
+
+const contract = new web3.eth.Contract(contractABI, contractAddress);
+
+async function storeProof() {
+  const documentHash = '0x1a5792b0d70c21041f5e6fc9df90564d231b038616306b39e1c008eb43fb36a6';
+  const accounts = await web3.eth.getAccounts();
+  contract.methods.storeProof(documentHash).send({from: accounts[0]})
+    .on('receipt', receipt => {
+      console.log(receipt);
+    })
+    .on('error', error => {
+      console.error(error);
+    });
+}
+
+async function getProof() {
+  console.log('clicked')
+  const documentHash = '0x1a5792b0d70c21041f5e6fc9df90564d231b038616306b39e1c008eb43fb36a6'
+  const timestamp = await contract.methods.getProof(documentHash).call();
+  console.log(timestamp)
 }
 
 // You can call this function when the window loads to auto-connect MetaMask
