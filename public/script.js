6d83483567912e7f2753221443a1b081cddc6870
Jake <jiv7145@gmail.com>
2023-10-20 18:51:40 -0700

    Fix bug for metamask

--- public/script.js
+++ public/script.js
@@ -1,22 +1,15 @@
-let web3 = new Web3(window.ethereum);
 async function connectMetaMask() {
-    // Initialize web3 instance
-    let web3 = new Web3(window.ethereum);
-
-
+  if (window.ethereum) {
     try {
-        // Request account access
-        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-
-        // Handle account connection
-        if (accounts.length > 0) {
-            // You now have access to the connected account(s)
-            console.log("Connected accounts:", accounts);
-        }
+      // Request account access
+      const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
+      console.log(accounts[0]); // Should show current MetaMask account
     } catch (error) {
-        // Handle error. Likely the user rejected the login
-        console.error("Error:", error);
+      console.error("User denied account access");
     }
+  } else {
+    console.error("MetaMask not installed");
+  }
 }
 
 const contractABI = [
@@ -73,6 +66,7 @@ const contractABI = [
   }
 ]
 
+let web3 = new Web3(window.ethereum);
 const contractAddress = '0x289EeF0cDa86fE61fb60f0A95C1b4A2295Dedbee'
 
 const contract = new web3.eth.Contract(contractABI, contractAddress);
@@ -133,28 +127,8 @@ async function getProof() {
   if (!documentHash.startsWith('0x')) {
     documentHash = '0x' + documentHash;
   }
-  
+
   // const documentHash = '0x1a5792b0d70c21041f5e6fc9df90564d231b038616306b39e1c008eb43fb36a6'
   const timestamp = await contract.methods.getProof(documentHash).call();
   console.log(timestamp)
 }
-
-// You can call this function when the window loads to auto-connect MetaMask
-window.addEventListener('load', async () => {
-    if (window.ethereum) {
-        // Initialize web3 instance
-        let web3 = new Web3(window.ethereum);
-
-        // Get list of connected accounts. Empty array if none are connected
-        const accounts = await window.ethereum.request({ method: 'eth_accounts' });
-
-        if (accounts.length !== 0) {
-            // MetaMask is already connected
-            console.log("MetaMask is already connected:", accounts);
-        } else {
-            // MetaMask is not connected
-            console.log("MetaMask is not connected");
-        }
-    }
-});
-
